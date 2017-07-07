f9bc68834ac4a34c954d0e1cb20f8007ffc11708
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2017-07-07 23:00:30 +0900

    問題への解答を実装

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,32 @@
 class BonusDrink
+  # 新しい飲み物を1本プレゼントしてもらえる空瓶の本数
+  BONUS_CONDITION_EMPTY_BOTTLE_COUNT = 3
+
+  # 不正な引数が与えられた場合の例外時に出力するメッセージ
+  ERROR_MESSAGE_AMOUNT_INVALID = "購入した本数には正の整数を指定してください"
+
   def self.total_count_for(amount)
-    0
+	if !numeric?(amount) || amount < 0
+		raise ERROR_MESSAGE_AMOUNT_INVALID
+	end
+	
+	total_count = amount
+	rest = amount
+	loop do
+		if (rest - BONUS_CONDITION_EMPTY_BOTTLE_COUNT) < 0
+			break
+		end
+		# 指定の本数分、新しい飲み物を1本プレゼントしてもらえる
+		rest -= BONUS_CONDITION_EMPTY_BOTTLE_COUNT
+		rest += 1
+		total_count += 1
+	end
+	total_count
   end
+
+  def self.numeric?(argument)
+	argument.to_s =~ /^[0-9]+$/
+  end
+
+  private_class_method :numeric?
 end
---
+++ call_bonus_drink.rb
@@ -0,0 +1,4 @@
+require File.expand_path(File.dirname(__FILE__) + '/bonus_drink')
+
+p BonusDrink.total_count_for(100)
+
